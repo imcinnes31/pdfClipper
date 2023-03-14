16ff5b051af5cb56d3e2f7d712c41b7874f54c24
imcinnes31 <imcinnes31@gmail.com>
2023-03-14 02:03:11 -0500

    initial commit

--- pdf_clipper/pdf_clipper/settings.py
+++ pdf_clipper/pdf_clipper/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = Path(BASE_DIR).joinpath('staticfiles')
-STATICFILES_DIRS = (Path(BASE_DIR).joinpath('static'),)
+STATICFILES_DIRS = (Path(BASE_DIR).joinpath('pdf_clip/static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
